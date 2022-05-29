8bf7defb952ed799edfda5b23996760371d7ae7e
Gabriel Oliveira <dev.gabriel.oliveira01@gmail.com>
2022-05-29 20:47:03 -0300

    melhoria na class Sql

--- class/Sql.php
+++ class/Sql.php
@@ -2,37 +2,20 @@
 
 class Sql {
 
-    function __Construct(){
+    private $conn;
 
-        $conn = new PDO("mysql:dbname=db_cadastrousuario;host=127.0.0.1", "root", "");
+    function __construct(){
+
+        $this->conn = new PDO("mysql:dbname=db_cadastrousuario;host=127.0.0.1", "root", "");
     }
 
     public function cadastraUsuario($username, $password, $email){
 
-        $conn = new PDO("mysql:dbname=db_cadastrousuario;host=127.0.0.1", "root", "");
-
-        $stmt = $conn->prepare("INSERT INTO tb_usuarios (usernameusuario, passwordussuario, emailusuario) VALUES('$username', '$password', '$email');");
+        $stmt = $this->conn->prepare("INSERT INTO tb_usuarios (usernameusuario, passwordussuario, emailusuario) VALUES('$username', '$password', '$email');");
 
         $stmt->execute();
     }
 
-    public function atualizaUsuario($nameUsuario, $newNameUsuario, $newSenhaUsuario, $newEmailUsuaio){
-
-    	$conn = new PDO("mysql:dbname=db_cadastrousuario;host=localhost", "root", "");
-
-    	$stmt = $conn->prepare("UPDATE tb_usuarios SET usernameusuario = '$newNameUsuario' WHERE usernameusuario = '$nameUsuario';");
-
-    	$stmt->execute();
-    }
-
-    // public function excluiUsuario($nameUsuario) {
-
-    // 	$conn = new PDO("mysql:dbname=db_cadastrousuario;host=127.0.0.1", "root", "");
-
-    // 	$stmt = $conn->prepare("DELETE FROM tb_usuarios WHERE usernameusuario = "$nameUsuario";");
-    // }
-
 }
-
 
 ?>
